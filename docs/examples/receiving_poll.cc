1bbb704f8d5831862de54735f2aaff972a6bc782
Kolja Schneider <kolja.schneider@solectrix.de>
2021-11-08 15:49:02 +0100

    hack example

--- docs/examples/receiving_poll.cc
+++ docs/examples/receiving_poll.cc
@@ -10,17 +10,30 @@ int main(void)
     uvgrtp::session *sess = ctx.create_session("127.0.0.1");
 
     /* See sending.cc for more details */
-    uvgrtp::media_stream *hevc = sess->create_stream(8888, 8889, RTP_FORMAT_H265, 0);
+    uvgrtp::media_stream *hevc = sess->create_stream(5006, 5006, RTP_FORMAT_GENERIC, 0);
 
     /* pull_frame() will block until a frame is received.
      *
      * If that is not acceptable, a separate thread for the reader should be created */
     uvgrtp::frame::rtp_frame *frame = nullptr;
 
-    while (!(frame = hevc->pull_frame())) {
-        /* When we receive a frame, the ownership of the frame belongs to us and
-         * when we're done with it, we need to deallocate the frame */
-        (void)uvgrtp::frame::dealloc_frame(frame);
+    while(1) {
+        while (!(frame = hevc->pull_frame())) {
+            /* When we receive a frame, the ownership of the frame belongs to us and
+             * when we're done with it, we need to deallocate the frame */
+            (void)uvgrtp::frame::dealloc_frame(frame);
+        }
+#if 0
+        for (size_t i = 0; i < frame->header.cc; ++i) {
+            printf("csrc%u: %08x\n", i, frame->csrc[i]);
+        }
+#endif
+        if (frame->ext) {
+            uint8_t * ext_hdr = frame->ext->data;
+		int width  = ext_hdr[2] | (ext_hdr[1] << 8);
+		int height = ext_hdr[5] | (ext_hdr[4] << 8);
+            printf("got some extension: %d:%d\n", width, height);
+        }
     }
 
     /* You can also specify for a timeout for the operation and if the a frame is not received
